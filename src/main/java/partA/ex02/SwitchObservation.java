c7e20d7ee9ef79cb0f7ed66bfb67247d6183c06c
Clamjosh <Clamjosh1@users.noreply.github.com>
2025-01-20 11:52:50 -0500

    I finished, just forgot to push

--- src/main/java/partA/ex02/SwitchObservation.java
+++ src/main/java/partA/ex02/SwitchObservation.java
@@ -3,33 +3,28 @@ package partA.ex02;
 public class SwitchObservation {
 
     public static String observation(){
-        String response ="";
+        String response = "";
 
         char option = 'A';
         int aCount = 0, bCount = 0, cCount = 0;
         switch (option) {
             case 'A':
                 aCount++;
-               response += ("Count of A  " + aCount);
-                break;
+                response += ("Count of A  " + aCount);
             case 'B':
                 bCount++;
-                response +=("Count of B  " + bCount);
-                break;
+                response += (" Count of B  " + bCount);
             case 'C':
                 cCount++;
-                response +=("Count of B  " + cCount);
+                response += (" Count of C  " + cCount);
                 break;
-
         }
-
 
         return response;
     }
+
     public static void main(String args[]) {
         String observationOutput = observation();
         System.out.println(observationOutput);
-
-
     }
 }
