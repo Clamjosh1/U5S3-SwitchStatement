1d4f383cb25b4491e449556afd4fce4227eb3b30
Clamjosh <Clamjosh1@users.noreply.github.com>
2025-01-24 12:41:27 -0500

    I finished

--- src/main/java/partB/ex01/IfLight.java
+++ src/main/java/partB/ex01/IfLight.java
@@ -1,21 +1,32 @@
 package partB.ex01;
 
+import java.util.Scanner;
+
 public class IfLight {
-    public static String ifLight(){
+    public static String ifLight() {
         String response = "";
 
-        /* Put your code in between these comments : Top */
+        // make a scanner to read input
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a color code:");
 
 
+        int currentColor = scanner.nextInt();
 
 
-
-        /* Put your code in between these comments : Bottom */
-
-
+        if (currentColor == 0) {
+            response = "Next Traffic Light is green";
+        } else if (currentColor == 1) {
+            response = "Next Traffic Light is yellow";
+        } else if (currentColor == 2) {
+            response = "Next Traffic Light is red";
+        } else {
+            response = "Invalid color";
+        }
 
         return response;
     }
+
     public static void main(String args[]) {
         String switchOutput = ifLight();
         System.out.println(switchOutput);
--- src/main/java/partB/ex02/SwitchLight.java
+++ src/main/java/partB/ex02/SwitchLight.java
@@ -1,18 +1,36 @@
 package partB.ex02;
 
+import java.util.Scanner;
+
 public class SwitchLight {
-    public static String switchLight(){
+    public static String switchLight() {
         String response = "";
+        Scanner scanner = new Scanner(System.in);
 
-        /* Put your code in between these comments : Top */
+        System.out.println("Enter a color code:");
+        int colorCode = scanner.nextInt();
 
-
-        /* Put your code in between these comments : Bottom */
-
-
+        switch (colorCode) {
+            case 1:
+                response = "Red light is on!";
+                break;
+            case 2:
+                response = "Green light is on!";
+                break;
+            case 3:
+                response = "Blue light is on!";
+                break;
+            case 0:
+                response = "Invalid color";
+                break;
+            default:
+                response = "Invalid color";
+                break;
+        }
 
         return response;
     }
+
     public static void main(String args[]) {
         String switchOutput = switchLight();
         System.out.println(switchOutput);
